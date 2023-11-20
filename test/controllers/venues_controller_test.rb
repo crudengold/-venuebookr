40e34f4706af7c91e21812703210b2c24c0db607
Alex Cruden <alexscruden@gmail.com>
2023-11-20 16:59:11 +0000

    renamed venues controller to plural

--- test/controllers/venues_controller_test.rb
+++ test/controllers/venues_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class VenueControllerTest < ActionDispatch::IntegrationTest
+class VenuesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
