034118c1a911fc5fa431c42529ed72b630e7cd18
Alex Cruden <alexscruden@gmail.com>
2023-11-20 16:37:13 +0000

    created venue model and venue controller with empty methods

---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,13 @@
+class VenuesController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_163218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "venues", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "price_per_hour"
+    t.string "name"
+    t.text "description"
+    t.string "location"
+    t.integer "capacity"
+  end
 
 end
