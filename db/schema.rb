1afe480eb7c184e3953cd60b2756cf030b384ac2
Michael Bailey <michaelbailey92@gmail.com>
2023-11-21 13:51:39 +0000

    added links to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_163218) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_165743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "venues", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "price_per_hour"
-    t.string "name"
-    t.text "description"
-    t.string "location"
-    t.integer "capacity"
-
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,7 +24,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_163218) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
+  create_table "venues", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "price_per_hour"
+    t.string "name"
+    t.text "description"
+    t.string "location"
+    t.integer "capacity"
   end
 
 end
