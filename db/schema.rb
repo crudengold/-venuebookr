4f734d6e03a1bd037c548d2b806280fba6d63205
Zeinab Warsama <swarsama@hotmail.co.uk>
2023-11-21 12:21:55 +0000

    Created a booking model with foreign keys + migrated to sceheme

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_163218) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_121947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "venues", force: :cascade do |t|
+  create_table "bookings", force: :cascade do |t|
+    t.integer "total_price"
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "venue_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "price_per_hour"
-    t.string "name"
-    t.text "description"
-    t.string "location"
-    t.integer "capacity"
-
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["venue_id"], name: "index_bookings_on_venue_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,7 +36,18 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_163218) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
+  create_table "venues", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "price_per_hour"
+    t.string "name"
+    t.text "description"
+    t.string "location"
+    t.integer "capacity"
+  end
+
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "venues"
 end
