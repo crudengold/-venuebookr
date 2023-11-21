707123edd62e71d4a7ac3b1d6e07bd2331583636
Zeinab Warsama <swarsama@hotmail.co.uk>
2023-11-21 14:51:42 +0000

    Added methods create, destroy into booking controller + added working progress simple form into booking new page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,33 @@
 class BookingsController < ApplicationController
 
+  #http://localhost:3000/venues/1/bookings/new
+
   def new
+    @venue = Venue.find(params[:venue_id])
     @booking = Booking.new
   end
 
   def create
+    @venue = Venue.find(params[:venue_id])
+    @booking = Booking.new(bookings_params)
+    @booking.venue = @booking
+    if @booking.save!
+      redirect_to list_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @booking = Boooking.find(params[:id])
+    @booking.destroy
+    redirect_to booking_path(@booking.venue), status: :see_other
+  end
+
+  private
+
+  def bookings_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,4 @@
 class Booking < ApplicationRecord
-  validates :start_date, :end_date, presence: true
   belongs_to :venue
   belongs_to :user
 end
