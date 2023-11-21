acc8ea91e5d7e9890867ae4fe63eac7cf5e3c3b9
Jonathan Grinstead <jonathangrinstead.design@gmail.com>
2023-11-21 12:54:20 +0000

    add venue index and venue show

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -7,6 +7,10 @@ class VenuesController < ApplicationController
     @venue = Venue.new
   end
 
+  def show
+    @venue = Venue.find(params[:id])
+  end
+
   def create
     @venue = Venue.new(venue_params)
     if @venue.save
