1c6552d930d79adcc803a3c82bdb0543920ef9a7
Alex Cruden <alexscruden@gmail.com>
2023-11-23 12:05:00 +0000

    styling map

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,5 @@
 class Venue < ApplicationRecord
-  validates :name, :location, :description, :capacity, :price_per_hour, presence: true
+  validates :name, :location, :description, :capacity, :photos, :price_per_hour, presence: true
   validates :description, length: { minimum: 50 }
   has_many_attached :photos
   has_many :bookings, dependent: :destroy
