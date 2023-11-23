13608d0bd2af9282fefbf81169d8a83cd710eac4
Alex Cruden <alexscruden@gmail.com>
2023-11-23 10:33:25 +0000

    geocoder now works on venue locations

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,4 +4,6 @@ class Venue < ApplicationRecord
   has_many_attached :photos
   has_many :bookings, dependent: :destroy
   belongs_to :user
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_162503) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_102631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_162503) do
     t.string "location"
     t.integer "capacity"
     t.bigint "user_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
